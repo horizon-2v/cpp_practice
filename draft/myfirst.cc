6c6faef9a621f3bed8c481ebdcb2b84bff27a412
horizon_2v <horizon_2v@163.com>
2023-11-20 00:08:39 +0800

    update note

--- draft/myfirst.cc
+++ draft/myfirst.cc
@@ -6,9 +6,10 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    constexpr int size = 10;
-    int arr[size] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int *beg = begin(arr), *last = end(arr);
-    cout << *beg << endl;
+    cout << argc << endl;
+    cout << argv[0] << endl;
+    if(argc > 1){
+        cout << argv[1] << endl;
+    }
     return 0;
 }
