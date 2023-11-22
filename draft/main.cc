2e2b05229de87e6b7dde8d1f4e93e4b078c791b5
horizon_2v <horizon_2v@163.com>
2023-11-22 22:17:47 +0800

    update note

--- draft/main.cc
+++ draft/main.cc
@@ -1,10 +1,19 @@
 #include <iostream>
 #include "test.h"
+#include <cassert>
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    function(3, 1, 2, 3);
+    int num;
+    cin >> num;
+    if(num > 5){
+        cerr << __DATE__ << " " << __TIME__ << endl
+        << "Error: " << __FILE__ << " in function "
+        << __func__ << " at line:" << __LINE__ << endl
+        << "input bigger than 5" << endl;
+    }
+
     return 0;
 }
